118a4f8f7c693c7a2104c77bc49bc9c58c00cff8
Gabriel Kenji <134003510+Jackoki@users.noreply.github.com>
2025-09-12 22:29:53 -0300

    Create karma.conf.js

---
+++ frontend/karma.conf.js
@@ -0,0 +1,14 @@
+// frontend/karma.conf.js
+module.exports = function(config) {
+  config.set({
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    browsers: ['ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    },
+    singleRun: true
+  });
+};
