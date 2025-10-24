96e7a466153091f3e19e23f7520fbb595260776c
Leonardo Ferreira Girotto Bueno <104871954+leonardofgirotto@users.noreply.github.com>
2025-10-24 01:55:07 -0300

    Ajustando cor dos botões

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { Login } from './components/login/login';
 import { Register } from './components/register/register';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/register', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: Login },
   { path: 'register', component: Register }
