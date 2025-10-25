ab941dbf91108bc7c60ba52d19fe9c733d9e7f2d
Jackoki <kenji.inoue@hotmail.com>
2025-10-25 11:14:31 -0300

    Correção do CD

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -1,23 +1,22 @@
 import { TestBed } from '@angular/core/testing';
-import { App } from './app';
+import { AppComponent } from './app';
 
-describe('App', () => {
+describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [AppComponent], // é standalone, então entra aqui
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it('should render', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     expect(fixture.nativeElement).toBeTruthy();
   });
-
 });
